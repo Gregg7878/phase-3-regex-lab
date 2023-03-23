af9a25a87fdb3aef1cdf73cc91309cbb8202b1be
Gregg7878 <theheartbreakkidd777@gmail.com>
2023-03-23 19:15:38 +0300

    finished the regex_lab.rb

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
-end
-
-def words_starting_with_un_and_ending_with_ing(text)
-
-end
-
-def words_five_letters_long(text)
-
-end
-
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
-def valid_phone_number?(phone)
-
-end
+    word.downcase.match(/\A[aeiou]/) != nil
+  end
+  
+  def words_starting_with_un_and_ending_with_ing(words_string)
+    words_string.scan(/\bun\w+ing\b/)
+  end
+  
+  def words_five_letters_long(words_string)
+    words_string.scan(/\b\w{5}\b/)
+  end
+  
+  def first_word_capitalized_and_ends_with_punctuation?(text)
+    text.match(/\A[A-Z].+[.!?]\z/) != nil
+  end
+  
+  def valid_phone_number?(phone)
+    phone.match(/\A\(?([0-9]{3})\)?[-.\s]?([0-9]{3})[-.\s]?([0-9]{4})\z/) != nil
+  end
+  
